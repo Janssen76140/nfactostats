a84221e191c32704a2f92156c3f30efad80ca504
Janssen PILLEBOUT <janssen.pillebout@gmail.com>
2020-01-14 09:16:24 +0100

    Création du HTML CSS : - Header - Main img - Section text-graph

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
-require('vendor/autoload.php');
-
-use Cocur\Slugify\Slugify;
-
-$slugify = new Slugify();
-echo $slugify->slugify('Hello World!');
 
 
 
 
 
+
+
+include ('inc/header.php'); ?>
+
+<section class="graph-texte">
+    <div class="text-left">
+    <h2>Lets take your <br> <span>Business</span> to Next Level</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Eos, libero. Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit. Eos, libero</p>
+    </div>
+    <img src="asset/img/graph.svg">
+</section>
